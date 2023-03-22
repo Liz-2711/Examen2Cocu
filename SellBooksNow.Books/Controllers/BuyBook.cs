348ff545632e4b0129bfea344c733dd3c2b236ec
Liz-11 <85639388+Liz-11@users.noreply.github.com>
2023-03-21 21:10:55 -0600

    change

---
+++ SellBooksNow.Books/Controllers/BuyBook.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SellBooksNow.Books.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BuyBook
+    {
+         if (Database.Numberocopies.Any(x => x.Isbn == book.Isbn))
+        {
+            return BadRequest();
+    }
+    var ShopDto = new BooktDataTransferObject
+    {
+        Isbn =Books.Isbn,
+
+     Title = Books.Tittle,
+
+   Pages = Books.Pages
+
+   Price = Book.Price
+
+     NumberOfCopies =0
+}
+    };
+    Database.Bookd(BookDto);
+
+        return Ok(BookDto);
+}
+}
